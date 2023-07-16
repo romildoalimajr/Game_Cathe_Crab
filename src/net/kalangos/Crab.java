21e0a82ed5350fd87f56f5afa589dc6c84ec5843
Romldo A. Lima Jr <odlimor.sevla@gmail.com>
2023-07-16 18:47:27 -0300

    Implementado a Sprite no jogo

--- src/net/kalangos/Crab.java
+++ src/net/kalangos/Crab.java
@@ -2,25 +2,34 @@ package net.kalangos;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 
 public class Crab {
 
 	public double x, y, dx, dy;
-	public double spd = 4;
+	public double speed = 4;
+	
+	public static BufferedImage[] crabSprite;
 
 	public Crab(int x, int y) {
 		this.x = x;
 		this.y = y;
+		double radius = Math.atan2((Game.HEIGHT/2 -20) - y, (Game.WIDTH/2 - 20) - x);
+		this.dx = Math.cos(radius);
+		this.dy = Math.sin(radius);
+		crabSprite = new BufferedImage[2];
+		crabSprite[0] = Game.spritesheet.getSprite(0, 0);
 		// TODO = Calculo até o buraco
 	}
 
 	public void update() {
-		x++;
-		y++;
+		x += dx * speed;
+		y += dy * speed;
 	}
 
 	public void render(Graphics g) {
-		g.setColor(Color.RED);
-		g.fillRect((int) x, (int) y, 48, 48);
+		g.drawImage(crabSprite[0], (int)x, (int)y, 40, 40, null);
+		//g.setColor(Color.RED);
+		//g.fillRect((int) x, (int) y, 48, 48);
 	}
 }
--- src/net/kalangos/Game.java
+++ src/net/kalangos/Game.java
@@ -10,52 +10,53 @@ import java.util.List;
 
 import javax.swing.JFrame;
 
-public class Game extends Canvas implements Runnable{
+public class Game extends Canvas implements Runnable {
 
 	public static int WIDTH = 480;
 	public static int HEIGHT = 480;
-	
+
 	public static List<Crab> crabs;
-	
+
 	public Spawner spawner;
-	
+
+	public static Spritesheet spritesheet;
+
 	public Game() {
 		this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-		
+		spritesheet = new Spritesheet("/spritesheet.png");
 		crabs = new ArrayList<>();
-		
+
 		spawner = new Spawner();
-		
-	
+
 	}
-	
+
 	public void update() {
 		spawner.update();
-		for(int i = 0; i < crabs.size(); i++) {
+		for (int i = 0; i < crabs.size(); i++) {
 			crabs.get(i).update();
 		}
 	}
-	
+
 	public void render() {
 		BufferStrategy bs = this.getBufferStrategy();
-		if(bs == null) {
+		if (bs == null) {
 			this.createBufferStrategy(3);
 			return;
 		}
 		Graphics g = bs.getDrawGraphics();
-		g.setColor(new Color(255,229,102));
+		g.setColor(new Color(255, 229, 102));
 		g.fillRect(0, 0, WIDTH, HEIGHT);
 		g.setColor(Color.black);
-		g.fillOval(WIDTH/2 - 35, HEIGHT/2 - 35, 70, 70);
-		
-		for(int i = 0; i < crabs.size(); i++) {
+		g.fillOval(WIDTH / 2 - 20, HEIGHT / 2 - 20, 40, 40);
+
+		for (int i = 0; i < crabs.size(); i++) {
 			crabs.get(i).render(g);
 		}
-		
+
 		g.dispose();
 		bs.show();
 	}
-	
+
 	public static void main(String[] args) {
 		Game game = new Game();
 		JFrame frame = new JFrame();
@@ -65,9 +66,9 @@ public class Game extends Canvas implements Runnable{
 		frame.pack();
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		frame.setVisible(true);
-		
+
 		new Thread(game).start();
 	}
 
@@ -75,11 +76,11 @@ public class Game extends Canvas implements Runnable{
 	public void run() {
 		// TODO Auto-generated method stub
 		double fps = 60.0;
-		while(true) {
+		while (true) {
 			update();
 			render();
 			try {
-				Thread.sleep((int)(1000/fps));
+				Thread.sleep((int) (1000 / fps));
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
